b688631fdfd340e0ca74947677583047022a193e
martinasartori97 <martinasartori9@gmail.com>
2024-10-30 16:44:27 +0100

    edit js

--- controllers/postscontrollers.js
+++ controllers/postscontrollers.js
@@ -1,32 +1,15 @@
-const posts = [
-    {
-        title: "Primo Post",
-        content: "contenuto del primo post",
-        image: "public/Harry Potter.webp",
-        tags: ["introduzione", "benvenuto"]
-    },
-    {
-        title: "Secondo Post",
-        content: "Contenuto del secondo post",
-        image: "public/harry-p.jpg",
-        tags: ["notizie", "aggiornamenti"]
-    },
-    {
-        title: "Terzo Post",
-        content: "Contenuto del terzo post",
-        image: "public/Harry-Potter-la-storia-di-vita-del-giovane-mago-Analisi-in-chiave-LIBET.jpg.jpg",
-        tags: ["tutorial", "guida"]
-    },
-    {
-        title: "Quarto Post",
-        content: "Contenuto del quarto post",
-        image: "public/Harry-Potter.webp",
-        tags: ["opinioni", "riflessioni"]
-    },
-    {
-        title: "Quinto Post",
-        content: "Contenuto del quinto post",
-        image: "public/images.jpg",
-        tags: ["divertimento", "intrattenimento"]
-    }
-];
+const posts = require('../data/posts.js');
+
+
+function index(req, res) {
+
+  res.json({
+    data: posts,
+    count: posts.length
+  })
+}
+
+
+module.exports = {
+  index
+}
