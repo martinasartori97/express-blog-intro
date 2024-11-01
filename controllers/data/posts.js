ca193fd9109855d68ebb359801a8390f5f84085f
martinasartori97 <martinasartori9@gmail.com>
2024-11-01 14:38:45 +0100

    edit js

--- controllers/data/posts.js
+++ controllers/data/posts.js
@@ -1,33 +1,33 @@
 const posts = [
     {
-        title: "Primo Post",
-        content: "contenuto del primo post",
+        title: "harry potter,Hermione Granger e Ron Weasley ",
+        content: " La loro Amicizia",
         image: "public/Harry Potter.webp",
-        tags: ["introduzione", "benvenuto"]
+        tags: ["amicizia", "avventure magiche"]
     },
     {
-        title: "Secondo Post",
-        content: "Contenuto del secondo post",
+        title: "harry potter,Hermione Granger e Ron Weasley da piccoli",
+        content: "Affrontare le Sfide",
         image: "public/harry-p.jpg",
-        tags: ["notizie", "aggiornamenti"]
+        tags: ["famiglia", "mondo mangico"]
     },
     {
-        title: "Terzo Post",
-        content: "Contenuto del terzo post",
+        title: "la storia di vita del giovane mago",
+        content: "Crescita Personale",
         image: "public/Harry-Potter-la-storia-di-vita-del-giovane-mago-Analisi-in-chiave-LIBET.jpg.jpg",
-        tags: ["tutorial", "guida"]
+        tags: ["crescita personale", "storia"]
     },
     {
-        title: "Quarto Post",
+        title: "harry potter in volo con la bacchetta magica",
         content: "Contenuto del quarto post",
         image: "public/Harry-Potter.webp",
-        tags: ["opinioni", "riflessioni"]
+        tags: ["avventura", "libertà"]
     },
     {
-        title: "Quinto Post",
-        content: "Contenuto del quinto post",
+        title: "harry potter,Hermione Granger e Ron Weasley da adulti",
+        content: "Che sia per una partita di Quidditch o per una semplice volata",
         image: "public/images.jpg",
-        tags: ["divertimento", "intrattenimento"]
+        tags: ["crescita", "avventure magiche"]
     }
 ];
 
